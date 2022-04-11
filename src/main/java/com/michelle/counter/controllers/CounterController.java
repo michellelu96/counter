cb88dcc79eeeb337bbce7cc3a1297fb14a4ce5e0
michellelu96 <lu.michelle96@icloud.com>
2022-04-11 11:27:10 -0700

    finished
    didn't do ninja bonus

--- src/main/java/com/michelle/counter/controllers/CounterController.java
+++ src/main/java/com/michelle/counter/controllers/CounterController.java
@@ -3,10 +3,29 @@ package com.michelle.counter.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CounterController {
-	public String index(HttpSession session) {
+	
+	@GetMapping("/")
+    public String index(HttpSession session) {
+		if (session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		}
+		else {
+			Integer currentCount =(Integer) session.getAttribute("count");
+			currentCount++;
+			session.setAttribute("count", currentCount);
+			
+		}
 		return "index.jsp";
+	}
+	
+	@GetMapping("/counter")
+	public String showCount(HttpSession session) {
+
+		return "ShowCount.jsp";
 	}
 }
--- src/main/webapp/WEB-INF/ShowCount.jsp
+++ src/main/webapp/WEB-INF/ShowCount.jsp
@@ -1,13 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Counter</title>
+<title>Show Count</title>
 </head>
 <body>
-	<h1>Welcome User!</h1>
+	<p>You have visited this page <c:out value="${count}"/> times</p>
+	<a href="/">Test Another Visit?</a>
 </body>
 </html>
